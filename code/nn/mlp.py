57dce2e1839924de331a83f9d7cbd876834ab0e8
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-12-13 11:58:40 +0000

    Train val split

--- code/nn/mlp.py
+++ code/nn/mlp.py
@@ -18,17 +18,18 @@ DERIVATIVE = {
 
 
 class MultilayerPerceptron:
-    def __init__(self, layers: list[tuple[int, str]], optimiser: Optimiser, l2_reg: float = 0.0) -> None:
+    def __init__(self, layers: list[tuple[int, str]], optimiser: Optimiser, l2_reg: float = 0.0, seed: int = 144) -> None:
         self.layers = layers  # [(layer size, activation function), ...]
         self.optimiser = optimiser
         self.weights = [None] * len(layers)
         self.biases = [None] * len(layers)
         self.x_mean, self.x_std = None, None
-        self.l2_reg = l2_reg  # L2-Regularisation lambda, recommended to set as 1e-2
+        self.l2_reg = l2_reg  # L2-Regularisation lambda, recommended to set as 1e-2 -- higher value => more regularisation
+        self.rng = np.random.default_rng(seed)  # random generator
         self._init_params()
 
 
-    def _init_params(self, seed: int = 144) -> None:
+    def _init_params(self) -> None:
         """
         Initialises the weights and biases of the neural network
 
@@ -37,7 +38,6 @@ class MultilayerPerceptron:
         """
         # Initialise weights and biases with random values
         # NOTE: 0-index will not be used
-        rng = np.random.default_rng(seed)  # random generator
         for i in range(1, len(self.layers)):
             prev_layer = self.layers[i-1][0]
             next_layer = self.layers[i][0]
@@ -50,7 +50,7 @@ class MultilayerPerceptron:
                 # Glorot initialisation
                 std = np.sqrt(2.0 / (prev_layer + next_layer))
 
-            w = rng.normal(0, std, size=(next_layer, prev_layer))
+            w = self.rng.normal(0, std, size=(next_layer, prev_layer))
             b = np.zeros((next_layer, 1))
             self.weights[i] = w
             self.biases[i] = b
@@ -188,6 +188,30 @@ class MultilayerPerceptron:
         return (xs - self.x_mean) / (self.x_std + 1e-8)
     
 
+    def _train_val_split(self, xs: np.ndarray, ys: np.ndarray, ratio: float = 0.7) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+        """
+        Split the input dataset into training and validation sections
+
+        Args:
+            xs: Input values
+            ys: Truth output values
+            ratio: Desired train-validation split -- e.g. 0.7 means 70% training, 30% validation
+
+        Returns:
+            Train dataset followed by validation dataset
+        """
+        # Shuffle data
+        idx = self.rng.permutation(len(xs))
+        xs, ys = xs[idx], ys[idx]
+
+        # Split data
+        split_idx = int(ratio * len(xs))
+        xs_train, xs_val = xs[:split_idx], xs[split_idx:]
+        ys_train, ys_val = ys[:split_idx], ys[split_idx:]
+
+        return xs_train, ys_train, xs_val, ys_val
+    
+
     def train(self, xs: np.ndarray, ys: np.ndarray, iterations: int, loss_goal: float = 1e-4) -> list[np.float64]:
         """
         Train the neural netwrok on a given data set
@@ -199,7 +223,7 @@ class MultilayerPerceptron:
             loss_goal: The early stopping threshold for training
 
         Returns:
-            A list of the total losses per iteration 
+            A list of the average losses per iteration 
         """
         # Normalise input
         self.x_mean, self.x_std = xs.mean(), xs.std()
@@ -207,7 +231,6 @@ class MultilayerPerceptron:
 
         # Train neural network
         self.optimiser.reset()
-        n = len(xs)
         losses = []
         for epoch in range(1, iterations+1):
             total_loss = 0
@@ -220,7 +243,7 @@ class MultilayerPerceptron:
                 grad_w, grad_b = self._backprop(cache, y)
                 self.optimiser.update(self.weights, self.biases, grad_w, grad_b)
             
-            mean_loss = total_loss / n
+            mean_loss = total_loss / len(xs)
             losses.append(mean_loss)
 
             if mean_loss < loss_goal:
