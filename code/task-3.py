c281f5bc9088828f192cf747e0f116ee591d6984
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-12-16 17:51:01 +0000

    Train the googlenet and alexnet models

--- code/task-3.py
+++ code/task-3.py
@@ -1,21 +1,28 @@
 import torch
 from torchvision import models, datasets, transforms
 from torch.utils.data import DataLoader, random_split
+from utils import plot_loss, plot_acc
 
 
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 
-def get_train_val_loaders() -> tuple[DataLoader, DataLoader, list[str]]:
+def get_train_val_loaders(foldername: str, ratio: float, seed: int = 42) -> tuple[DataLoader, DataLoader, list[str]]:
     """
     Get the training and validation data loaders for the fruits dataset
+
+    Args:
+        seed: Random seed for reproducibility
+        foldername: Name of the dataset folder 
+        ratio: Desired split -- e.g. 0.7 means a 70% / 30% split
 
     Returns:
         A tuple containing the training and validation data loaders, as well as the class names
     """
     # Transform dataset to make compatible with the models
     transform = transforms.Compose([
-        transforms.Resize((224, 224)),
+        transforms.Resize(256),
+        transforms.CenterCrop(224),  # The original AlexNet paper points to using 227, but the PyTorch implementation corrects this to 224
         transforms.ToTensor(),
         transforms.Normalize(
             # Mean and standard deviation of the ImageNet dataset
@@ -24,15 +31,13 @@ def get_train_val_loaders() -> tuple[DataLoader, DataLoader, list[str]]:
         )
     ])
 
-    foldername = 'task-3-fruits'
     dataset = datasets.ImageFolder(foldername, transform=transform)
 
     # Split dataset
-    split = 0.7
-    train_size = int(split * len(dataset))
+    train_size = int(ratio * len(dataset))
     val_size = len(dataset) - train_size
 
-    train_dataset, val_dataset = random_split(dataset, [train_size, val_size], generator=torch.Generator().manual_seed(42))
+    train_dataset, val_dataset = random_split(dataset, [train_size, val_size], generator=torch.Generator().manual_seed(seed))
 
     # Create data loaders
     train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True)
@@ -42,12 +47,12 @@ def get_train_val_loaders() -> tuple[DataLoader, DataLoader, list[str]]:
     return train_loader, val_loader, class_names
 
 
-def get_alexnet(num_classes: int) -> models.AlexNet:
+def get_alexnet(output_classes: int) -> models.AlexNet:
     """
     Get the AlexNet model, with the final classifier layer adjusted
 
     Args:
-        num_classes: The new number of classes in the final layer
+        output_classes: The new number of classes in the final layer
 
     Returns:
         The AlexNet model
@@ -61,18 +66,18 @@ def get_alexnet(num_classes: int) -> models.AlexNet:
 
     # Replace final classifier layer
     num_features = alexnet.classifier[-1].in_features
-    alexnet.classifier[-1] = torch.nn.Linear(num_features, num_classes)
+    alexnet.classifier[-1] = torch.nn.Linear(num_features, output_classes)
 
     alexnet = alexnet.to(DEVICE)
     return alexnet
 
 
-def get_googlenet(num_classes: int) -> models.GoogLeNet:
+def get_googlenet(output_classes: int) -> models.GoogLeNet:
     """
     Get the GoogLeNet model, with the final classifier layer adjusted
 
     Args:
-        num_classes: The new number of classes in the final layer
+        output_classes: The new number of classes in the final layer
 
     Returns:
         The GoogLeNet model
@@ -86,28 +91,10 @@ def get_googlenet(num_classes: int) -> models.GoogLeNet:
 
     # Replace final fully connected layer
     num_features = googlenet.fc.in_features
-    googlenet.fc = torch.nn.Linear(num_features, num_classes)
+    googlenet.fc = torch.nn.Linear(num_features, output_classes)
 
     googlenet = googlenet.to(DEVICE)
     return googlenet
-
-
-def get_optimiser(model: torch.nn.Module) -> torch.optim.Optimizer:
-    """
-    Get the optimiser to be used with the model
-
-    Args:
-        model: The neural network model
-
-    Returns:
-        The optimiser
-    """
-    optimiser = torch.optim.Adam(
-        filter(lambda p: p.requires_grad, model.parameters()),
-        lr=1e-3
-    )
-
-    return optimiser
 
 
 def train(model: torch.nn.Module, loader: DataLoader, criterion: torch.nn.Module, optimiser: torch.optim.Optimizer) -> tuple[float, float]:
@@ -177,7 +164,69 @@ def validate(model: torch.nn.Module, loader: DataLoader, criterion: torch.nn.Mod
     return running_loss / len(loader), correct / total
 
 
-train_loader, val_loader, classes = get_train_val_loaders()
-criterion = torch.nn.CrossEntropyLoss()
-googlenet = get_googlenet(len(classes))
-train(googlenet, train_loader, criterion, get_optimiser(googlenet))
+def train_model(model: torch.nn.Module, train_loader: DataLoader, val_loader: DataLoader, epochs: int) -> tuple[list[float], list[float], list[float], list[float]]:
+    """
+    Train the model on the training data and validate
+
+    Args:
+        model: The neural network model
+        train_loader: The training data loader
+        val_loader: The validation data loader
+        epochs: The number of iterations to train for
+
+    Returns:
+        The losses and accuracy of the model during the different epochs on the training and validation data
+    """
+    print(f'=== Training {type(model).__name__} ===')
+
+    criterion = torch.nn.CrossEntropyLoss()
+    optimiser = torch.optim.Adam(
+        filter(lambda p: p.requires_grad, model.parameters()),
+        lr=1e-3
+    )
+
+    train_losses = []
+    train_accs = []
+    val_losses = []
+    val_accs = []
+    for epoch in range(1, epochs+1):
+        train_loss, train_acc = train(model, train_loader, criterion, optimiser)
+        val_loss, val_acc = validate(model, val_loader, criterion)
+
+        train_losses.append(train_loss)
+        train_accs.append(train_acc)
+        val_losses.append(val_loss)
+        val_accs.append(val_acc)
+
+        print(
+            f'Epoch {epoch}/{epochs} | '
+            f'Train Loss: {train_loss:.4f}, Train Acc: {train_acc:.4f} | '
+            f'Val Loss: {val_loss:.4f}, Val Acc: {val_acc:.4f}'
+        )
+    
+    print()
+
+    return train_losses, train_accs, val_losses, val_accs
+
+
+if __name__ == "__main__":
+    # Train AlexNet and GoogleLeNet on the fruits dataset
+    foldername = 'task-3-fruits'
+    split = 0.7
+    train_loader, val_loader, classes = get_train_val_loaders(foldername, ratio=split)
+    num_classes = len(classes)
+
+    print(f'Dataset: "{foldername}" | Classes: {classes}\n')
+
+    iterations = 15
+
+    alexnet = get_alexnet(output_classes=num_classes)
+    a_train_losses, a_train_accs, a_val_losses, a_val_accs = train_model(alexnet, train_loader, val_loader, epochs=iterations)
+
+    googlenet = get_googlenet(output_classes=num_classes)
+    g_train_losses, g_train_accs, g_val_losses, g_val_accs = train_model(googlenet, train_loader, val_loader, epochs=iterations)
+    
+    plot_loss(a_train_losses, a_val_losses)
+    plot_loss(g_train_losses, g_val_losses)
+    plot_acc(a_train_accs, a_val_accs)
+    plot_acc(g_train_accs, g_val_accs)
--- code/utils/plot.py
+++ code/utils/plot.py
@@ -19,6 +19,17 @@ def plot_loss(train_ys: np.ndarray, val_ys: np.ndarray = []) -> None:
     plt.show()
 
 
+def plot_acc(train_ys: np.ndarray, val_ys: np.ndarray = []) -> None:
+    plt.figure()
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+    plt.plot(range(1, len(train_ys)+1), train_ys, label="Training")  # start from 1
+    if len(val_ys):
+        plt.plot(range(1, len(val_ys)+1), val_ys, label="Validation")
+    plt.legend()
+    plt.show()
+
+
 def plot_prediction(pred: tuple[np.ndarray, np.ndarray], actual: tuple[np.ndarray, np.ndarray]) -> None:
     plt.figure()
     plt.plot(*actual, 'k+', label="Actual")
