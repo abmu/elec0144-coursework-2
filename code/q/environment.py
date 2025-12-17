85ec8608de809293ee4ac2cd7bf58584ec29701b
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-12-17 20:38:20 +0000

    Added choose action, update q, and helper functions

--- code/q/environment.py
+++ code/q/environment.py
@@ -32,7 +32,7 @@ class Environment:
         """
         for i in range(self.rows):
             for j in range(self.cols):
-                if self.grid[i][j] == self.START:
+                if self._is_start((i, j)):
                     return (i, j)
         return (0, 0)  # default start
     
@@ -58,47 +58,23 @@ class Environment:
             return False, f'Invalid action. "{action}" is not in allowed actions: {self.ACTIONS.keys()}'
         
         # Check if currently in a terminal state
-        if self.is_terminal():
+        if self.is_terminal(self.pos):
             return False, 'Cannot move from a terminal position'
         
         dx, dy = self.ACTIONS[action]
         new_x, new_y = self.pos[0] + dx, self.pos[1] + dy
 
         # Check if within bounds
-        if not (0 <= new_x < self.rows and 0 <= new_y < self.cols):
+        if not self._in_bounds((new_x, new_y)):
             return False, 'Move out of bounds'
         
         # Check if obstacle
-        if self.grid[new_x][new_y] == self.OBSTACLE:
+        if self._is_obstacle((new_x, new_y)):
             return False, 'Cannot move into obstacle'
         
         # Update pos
         self.pos = (new_x, new_y)
         return True, None
-
-
-    def is_terminal(self) -> bool:
-        """
-        Checks if the current position is a terminal cell
-
-        Returns:
-            A boolean value determining if the current position is terminal
-        """
-        cell = self.grid[self.pos[0]][self.pos[1]]
-        return cell[0] == self.TERMINAL
-    
-
-    def cell_value(self) -> float:
-        """
-        Get the current value of the cell occupied
-
-        Returns:
-            A float representing the current cell value
-        """
-        cell = self.grid[self.pos[0]][self.pos[1]]
-        if cell[0] == self.START or cell[0] == self.TERMINAL:
-            cell = cell[1:]
-        return float(cell)
     
 
     def get_pos(self) -> tuple[int, int]:
@@ -109,3 +85,79 @@ class Environment:
             A tuple which is the current position
         """
         return self.pos
+    
+
+    def _in_bounds(self, pos) -> bool:
+        """
+        Check if the position is in bounds
+
+        Args:
+            pos: Position in the grid
+
+        Returns:
+            A boolean
+        """
+        x, y = pos
+        return 0 <= x < self.rows and 0 <= y < self.cols
+    
+
+    def _is_start(self, pos) -> bool:
+        """
+        Checks if the position is the start cell
+
+        Args:
+            pos: Position in the grid
+
+        Returns:
+            A boolean value determining if the position is the start
+        """
+        cell = self.grid[pos[0]][pos[1]]
+        return cell[0] == self.START
+
+
+    def _is_obstacle(self, pos) -> bool:
+        """
+        Checks if the position is an obstacle cell
+
+        Args:
+            pos: Position in the grid
+
+        Returns:
+            A boolean value determining if the position is an obstacle
+        """
+        cell = self.grid[pos[0]][pos[1]]
+        return cell[0] == self.OBSTACLE
+
+
+    def is_terminal(self, pos) -> bool:
+        """
+        Checks if the position is a terminal cell
+
+        Args:
+            pos: Position in the grid
+
+        Returns:
+            A boolean value determining if the position is terminal
+        """
+        cell = self.grid[pos[0]][pos[1]]
+        return cell[0] == self.TERMINAL
+    
+
+    def cell_value(self, pos) -> float:
+        """
+        Get the value of the cell at the input position
+
+        Args:
+            pos: Position in the grid
+
+        Returns:
+            A float representing the cell value
+        """
+        if not self._in_bounds(pos) or self._is_obstacle(pos):
+            return float('nan')
+
+        cell = self.grid[pos[0]][pos[1]]
+        if self._is_start(pos) or self.is_terminal(pos):
+            cell = cell[1:]  # remove start/terminal marker character at the beginning
+        return float(cell)
+    
--- code/q/tabular.py
+++ code/q/tabular.py
@@ -1,4 +1,5 @@
 import math
+import random
 from environment import Environment
 
 
@@ -15,7 +16,7 @@ class QLearning:
         self.epsilon_min = 0.01
 
     
-    def epsilon(self, episode: float) -> float:
+    def epsilon(self, episode: int) -> float:
         """
         Get the exponential decay epsilon value
 
@@ -40,3 +41,48 @@ class QLearning:
             Q-value of the state and action combination
         """
         return self.q_table.get((state, action), 0.0)
+    
+
+    def choose_action(self, state: tuple[int, int], episode: int) -> str:
+        """
+        Chooses an action using an epsilon-greedy policy
+
+        Args:
+            state: Current state in the environment
+            episode: Current episode
+
+        Returns:
+            An action to take
+        """
+        actions = self.env.ACTIONS.keys()
+        if random.random() < self.epsilon(episode):
+            # exploration - choose random action
+            return random.choice(actions)
+        else:
+            # exploitation - choose action with highest Q-value
+            qs = (self.get_q(state, a) for a in actions)
+            max_idx, max_q = max(enumerate(qs), key=lambda x: x[1])
+            return actions[max_idx]
+    
+    
+    def update_q(self, state: tuple[int, int], action: str, reward: float, next_state: tuple[int, int]) -> None:
+        """
+        Update the Q table using the reward given for action chosen in the previous state
+
+        Args:
+            state: Previous state in the environment
+            action: Pending action to perform
+            reward: Reward given for action chosen in the previous state
+            next_state: New state after the action was performed
+        """
+        if self.env.is_terminal():
+            best_next = 0.0
+        else:
+            actions = self.env.ACTIONS.keys()
+            best_next = max(self.get_q(next_state, a) for a in actions)
+        
+        old_q = self.get_q(state, action)
+
+        sample = reward + self.gamma * best_next
+        bellman_err = sample - old_q
+        self.q_table[(state, action)] = old_q + self.alpha * bellman_err
