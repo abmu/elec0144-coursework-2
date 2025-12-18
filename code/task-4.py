06c096197f240ba911a1beeefb2450c89ff5dbb9
Abdul Muhaymin Abdul Hafiz <mr-abdul@live.co.uk>
2025-12-18 21:25:06 +0000

    Added comment

--- code/task-4.py
+++ code/task-4.py
@@ -2,10 +2,12 @@ from q import Environment, QLearning
 from utils import parse_grid_data, plot_acc
 
 
+# Create environment from grid file
 filename = 'task-4-grid.txt'
 grid = parse_grid_data(filename)
 environment = Environment(grid)
 
+# Initialise q-learning algorithm
 q_learning = QLearning(
     env=environment,
     alpha=0.1,
@@ -13,7 +15,10 @@ q_learning = QLearning(
     epsilon_decay=0.995
 )
 
+# Train the q-learning algorithm
 rewards = q_learning.train(iterations=100)
+
+# Display results
 plot_acc(rewards)
 
 print(environment)
